ec4911e045b1308119439943be8cbcb4364872ae
Нестеров Сергей <ipnesterov@gmail.com>
2023-01-09 10:18:20 +0300

    Create config.py

---
+++ .github/рабочие процессы/config.py
@@ -0,0 +1,12 @@
+# Токен нашего бота
+tokenbot = '1111111111:AAAAAaaaaaAAAAAaaaaa'
+
+# Адрес для работы с API 5
+ReportsURL5 = "https://api.direct.yandex.com/json/v5/reports"
+# Адрес для работы с API 4 Live
+ReportsURL4 = "https://api.direct.yandex.ru/live/v4/json/"
+
+# Список наших логинов и токенов
+login_token = {'login1':'token1','login2':'token2'}
+# Chat_ID людей у кого есть разрешения
+chat_id = ['11111111','222222222']
